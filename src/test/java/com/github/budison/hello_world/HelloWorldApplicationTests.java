ebeec8a9abb69c4542a44e3407598911b2e2e272
Kevin <nowakk96@icloud.com>
2022-04-17 17:20:13 +0200

    Initial

---
+++ src/test/java/com/github/budison/hello_world/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.budison.hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
